2566851a076417b9b1db9448efb78f0d640e24b5
erickcartiady <erickcartiady@gmail.com>
2020-05-17 17:30:14 +0700

    Add Footer

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -5,6 +5,7 @@ import { TodoDate } from "./TodoDate";
 import { TodoList } from "./TodoList";
 import useTodoState from "../hooks/useTodoState";
 import { makeStyles } from "@material-ui/core/styles";
+import Footer from "./Footer";
 
 import {
   Paper,
@@ -40,38 +41,41 @@ function TodoApp() {
   const classes = useStyles();
 
   return (
-    <Grid container justify="center" style={{ height: "100vh" }}>
-      <Grid item xs={11}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6">TodayList</Typography>
-          </Toolbar>
-        </AppBar>
-        <Toolbar />
-        <div className={classes.toolbar} />
-        <Box
-          height="70vh"
-          display="flex"
-          flexDirection="column"
-          style={{ border: "1px solid black", padding: "1rem" }}
-        >
-          <Box style={{ marginBottom: "1rem" }}>
-            <TodoDate />
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">TodayList</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" style={{ height: "100vh" }}>
+        <Grid item xs={11}>
+          <Toolbar />
+          <div className={classes.toolbar} />
+          <Box
+            height="70vh"
+            display="flex"
+            flexDirection="column"
+            style={{ border: "1px solid black", padding: "1rem" }}
+          >
+            <Box style={{ marginBottom: "1rem" }}>
+              <TodoDate />
+            </Box>
+            <Box style={{ marginBottom: "1rem" }}>
+              <TodoForm addTodo={addTodo} />
+            </Box>
+            <Box height="72.5%">
+              <TodoList
+                todos={todos}
+                updateTodo={updateTodo}
+                deleteTodo={deleteTodo}
+                toggleComplete={toggleComplete}
+              />
+            </Box>
           </Box>
-          <Box style={{ marginBottom: "1rem" }}>
-            <TodoForm addTodo={addTodo} />
-          </Box>
-          <Box height="72.5%">
-            <TodoList
-              todos={todos}
-              updateTodo={updateTodo}
-              deleteTodo={deleteTodo}
-              toggleComplete={toggleComplete}
-            />
-          </Box>
-        </Box>
+        </Grid>
       </Grid>
-    </Grid>
+      <Footer />
+    </>
   );
 }
 
