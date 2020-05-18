d5f8371fa1188647432c3be553a2bc2561f29e99
erickcartiady <erickcartiady@gmail.com>
2020-05-18 13:04:21 +0700

    Updates

---
+++ precache-manifest.76260a06c695d0aacccdda1181a219f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e124922167bc65c30ff1bbb25f67324",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "2c302c96a11906451479",
+    "url": "/Todo-List/static/css/main.c1c30dca.chunk.css"
+  },
+  {
+    "revision": "e4667007170bb78e37a6",
+    "url": "/Todo-List/static/js/2.4b641d77.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-List/static/js/2.4b641d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c302c96a11906451479",
+    "url": "/Todo-List/static/js/main.f6c673ef.chunk.js"
+  },
+  {
+    "revision": "d6a2c1c1623c1cbc81ae",
+    "url": "/Todo-List/static/js/runtime-main.f4831e10.js"
+  }
+]);
